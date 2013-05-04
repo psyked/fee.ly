164c476c5a446363979d92f491be6078129c8e70
James Ford <jford@psyked.co.uk>
2013-05-04 22:17:23 +0100

    Configurated styling for the input field.

--- js/main.js
+++ js/main.js
@@ -1,3 +1,13 @@
-$(function() {
-    $(".dial").knob();
+$(function () {
+    $(".dial").knob({
+        fgColor: '#08C9C6',
+        bgColor: '#CCCCCC',
+        angleOffset: -125,
+        angleArc: 250,
+        min: 0,
+        max: 100,
+        width: 240,
+        height: 240,
+        cursor: 20
+    });
 });
