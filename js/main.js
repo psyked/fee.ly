0fb8d15d3fc764852e8e40a46ba5cb8ce14bfb97
James Ford <jford@psyked.co.uk>
2013-05-05 18:21:42 +0100

    Merged the plugins javascript and extensions to make the load more optimal, and added tags to remove the toolbar from the bottom of the app.

--- js/main.js
+++ js/main.js
@@ -12,3 +12,22 @@ $(function () {
         displayInput: false
     });
 });
+
+function hideAddressBar()
+{
+    if(!window.location.hash)
+    {
+        if(document.height < window.outerHeight)
+        {
+            document.body.style.height = (window.outerHeight + 50) + 'px';
+        }
+
+        setTimeout( function(){ window.scrollTo(0, 1); }, 50 );
+    }
+}
+
+window.addEventListener("load", function(){ if(!window.pageYOffset){ hideAddressBar(); } } );
+window.addEventListener("orientationchange", hideAddressBar );
+
+var st = sidetap();
+$(".header-button.menu").on("click",st.toggle_nav)
